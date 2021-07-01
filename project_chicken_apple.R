d8ce12b0cd060aec8a877efb3215ea9887e34e69
larscaspersen <larscaspersen@outlook.de>
2021-07-01 13:33:09 +0200

    added variables on events (insect vole, etc) and the share of additional costs. also made parts of code easier to read

--- project_chicken_apple.R
+++ project_chicken_apple.R
@@ -20,9 +20,6 @@ make_variables(estimate_read_csv(input_table))
 #set the path for model outputs
 dir.create(evpi_results_folder)
 
-#calculate the total apple yield (shouldn't this be inside the function?)
-harvest_total<-orchard_area*trees_per_ha*harvest_tree
-
 # We assume: An orchard with 10ha apples, 3000-4000 trees per ha, 
 # and one chicken coop with 1200 laying hens, conventional conditions
 
@@ -33,136 +30,121 @@ Apple_Simulation<- function(){
   #Costs apple orchard----
   
   #set the cost to run machine for one hour
-  cost_diesel<-consumption_diesel_hour*diesel_price
+  cost_diesel <- consumption_diesel_hour * diesel_price
   
   #cost to harvest the apples = time it takes to harvest * (wage + diesel_costs)
-  cost_harvesting<-(vv(yearly_harvesting_hours, var_CV, n=n_years)*
-    hourly_wage)+
-    (vv(yearly_harvesting_hours, var_CV, n=n_years)*
-    cost_diesel)
-    
+  cost_harvesting <- (vv(yearly_harvesting_hours, var_CV, n=n_years) * hourly_wage) +
+    (vv(yearly_harvesting_hours, var_CV, n=n_years) * cost_diesel)
+  
   #cost to mow the grass
-  cost_mowing<-(vv(yearly_mowing_hours, var_CV, n=n_years)*
-    hourly_wage)+
-    (vv(yearly_mowing_hours, var_CV, n=n_years)*
-    cost_diesel)
+  cost_mowing <- (vv(yearly_mowing_hours, var_CV, n=n_years) * hourly_wage) +
+    (vv(yearly_mowing_hours, var_CV, n=n_years) * cost_diesel)
   
   #cost to remove the weed from the strip
-  cost_weeding<-(vv(yearly_weeding_hours, var_CV, n=n_years)*
-    hourly_wage)+(vv(yearly_weeding_hours, var_CV, n=n_years)*
-    cost_diesel)+ 
-    cost_herbicide*
-    orchard_area*2/3
- 
+  cost_weeding <- (vv(yearly_weeding_hours, var_CV, n=n_years) * hourly_wage) + 
+    (vv(yearly_weeding_hours, var_CV, n=n_years) * cost_diesel) + 
+    cost_herbicide * orchard_area * 2/3
+  
   #cost to control for apple scab
-  cost_scabcontrol<-(vv(yearly_scabcontrol_hours, var_CV, n=n_years)*
-    hourly_wage)+
-    cost_fungicide*
-    orchard_area*2/3+
-    (vv(yearly_scabcontrol_hours, var_CV, n=n_years)*
-    cost_diesel)
+  cost_scabcontrol <- (vv(yearly_scabcontrol_hours, var_CV, n=n_years) * hourly_wage) +
+    cost_fungicide * orchard_area * 2/3 +
+    (vv(yearly_scabcontrol_hours, var_CV, n=n_years) *  cost_diesel)
+  
   
   #event of extraordinary apple scab infestation (chance of 25%, should be a variable)
-  #also, why is there the 0.3? The extraordinary apple scab event takes and additional 30% of time?
-  scab_year_treatment<-(vv(yearly_scabcontrol_hours, var_CV, n=1)*0.3*
-    hourly_wage)+
-    cost_fungicide*0.3*
-    orchard_area*2/3+
-    yearly_scabcontrol_hours*0.3*
-    cost_diesel  
-  cost_scab_year<-chance_event(0.25,scab_year_treatment, 0, n=n_years, 0.25)
+  #changed the 0.3 to a variable in the input table
+  #reformulated the equations so that the factor (0.3) is present only once
+  scab_year_treatment <- (vv(yearly_scabcontrol_hours, var_CV, n=1) * hourly_wage +
+                            cost_fungicide * orchard_area * 2/3 +
+                            yearly_scabcontrol_hours *cost_diesel) * proportion_extracost_apple_scab  
+  
+  #what was the use the last 0.25? didn't look like it changed anything
+  cost_scab_year <- chance_event(chance_intense_apple_scab,scab_year_treatment, 0, n=n_years)
   
   #cost of insect control
   #insects are confused with pheromones, so its a one time application
-  cost_insectcontrol<-vv(yearly_insectcontrol_hours, var_CV, n=n_years)*
-    hourly_wage+
-    cost_insecticide*
-    orchard_area+
-    vv(yearly_insectcontrol_hours, var_CV, n=n_years)*
-    cost_diesel
+  cost_insectcontrol <- vv(yearly_insectcontrol_hours, var_CV, n=n_years) * hourly_wage +
+    cost_insecticide * orchard_area +
+    vv(yearly_insectcontrol_hours, var_CV, n=n_years) * cost_diesel
   
   #event of extraordinary insect infestation
-  insect_year_treatment<-yearly_insectcontrol_hours*0.3*
-    hourly_wage+
-    cost_insecticide*0.3*
-    orchard_area*2/3+
-    yearly_insectcontrol_hours*0.3*
-    cost_diesel  
-  cost_insect_year<-chance_event(0.25,insect_year_treatment, 0, n=n_years, 0.25)
+  #reformulated it so that the 0.3 is a variable and appears only once
+  insect_year_treatment <- (yearly_insectcontrol_hours * hourly_wage +
+                              cost_insecticide * orchard_area * 2/3 +
+                              yearly_insectcontrol_hours * cost_diesel) * proportion_extracost_insect_infestation
   
-#the chances of both events (intensive apple scab and intensive insect infestation)
-#what are they based on? Also events like these every four years seem a lot to me
-#maybe every 10 years? This should definately be a variable in the model
+  #draw chances that years are heavily infested by insects
+  cost_insect_year<-chance_event(chance_intense_insect_infestation,insect_year_treatment, 0, n = n_years)
   
-  #cost of nutrient application
-  cost_nutrients<-(vv(yearly_fertilization_hours, var_CV, n=n_years)*
-    hourly_wage)+
-    cost_fertilizer*
-    orchard_area*2/3+ 
-    (vv(yearly_fertilization_hours, var_CV, n=n_years)*
-    cost_diesel)
+  #original code
+  # cost_nutrients<-(vv(yearly_fertilization_hours, var_CV, n=n_years)*
+  #   hourly_wage)+
+  #   cost_fertilizer*
+  #   orchard_area*2/3+ 
+  #   (vv(yearly_fertilization_hours, var_CV, n=n_years)*
+  #   cost_diesel)
+  
+  #my suggestion
+  #yearly cost to apply nutrients
+  cost_nutrients <- (vv(yearly_fertilization_hours, var_CV, n = n_years) * hourly_wage) + 
+    cost_fertilizer * orchard_area * 2/3 +
+    (vv(yearly_fertilization_hours, var_CV, n = n_years) * cost_diesel)
+  
   
   #vole control costs
-  cost_vole_control<-vv(yearly_vole_control_hours, var_CV, n=n_years)*
-    hourly_wage  
+  cost_vole_control <- vv(yearly_vole_control_hours, var_CV, n = n_years) * hourly_wage  
   
   #trees dying because of the voles?
-  dead_tree_occurrence<-vv(dead_trees, var_CV, n=1)
+  dead_tree_occurrence <- vv(dead_trees, var_CV, n = 1)
   
-  #cost to replace dead trees
-  cost_replacement_trees<-cost_tree*dead_trees+planting_hours_tree*dead_trees*hourly_wage
+  #cost to replace dead trees in a year
+  cost_replacement_trees <- cost_tree * dead_trees + planting_hours_tree * dead_trees * hourly_wage
   
   #chance of vole damage, chance should be definately be a variable
-  occurence_vole_damage<-chance_event(0.3,1,0, n=n_years)
-  #--> should the cost of replacing trees be even higher at vole events? 
-  #do we know how many trees voles damage at this event?
+  occurence_vole_damage <- chance_event(chance_intense_vole_damage, 1, 0, n = n_years)
   
-  #right know this for loop doesnt make a difference because you assign the exact values again to cost-replacement_tree
+  #cost the voles create by damaging trees and having to replace them
+  #ocurs only years when voles damage trees, otherwise zero
+  cost_voles <- occurence_vole_damage * cost_replacement_trees
+  
+  
+  #Sum up all costs----
+  Costs_apple_production <- cost_harvesting + cost_mowing + cost_weeding +
+    cost_insectcontrol + cost_insect_year +
+    cost_scabcontrol + cost_scab_year +
+    cost_vole_control + cost_nutrients +
+    cost_voles
+  
+  #--> isnt this the exact same loop as above?
   # for(i in 1:n_years){
   #   
   #   if(occurence_vole_damage[i] == 1){
   #     #if vole damage happens, some trees need to be replaced and affected trees 
   #     #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
   #     
-  #     #harvest_total[i]<-harvest_total-(dead_tree_occurrence*harvest_tree)
-  #     
-  #     cost_replacement_trees[i]<-cost_tree*dead_trees+planting_hours_tree*dead_trees*hourly_wage
+  #     cost_replacement_trees[i]<-
+  #       cost_tree*dead_trees+
+  #       planting_hours_tree*dead_trees*hourly_wage
   #   }
   #   
   # }
   
-#Sum up all costs----
-  Costs_apple_production <- cost_harvesting + cost_mowing + cost_weeding +
-                              cost_insectcontrol + cost_insect_year +
-                              cost_scabcontrol + cost_scab_year +
-                              cost_vole_control + cost_nutrients +
-                              occurence_vole_damage
   
-    #--> isnt this the exact same loop as above?
-    # for(i in 1:n_years){
-    #   
-    #   if(occurence_vole_damage[i] == 1){
-    #     #if vole damage happens, some trees need to be replaced and affected trees 
-    #     #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
-    #     
-    #     cost_replacement_trees[i]<-
-    #       cost_tree*dead_trees+
-    #       planting_hours_tree*dead_trees*hourly_wage
-    #   }
-    #   
-    # }
+  #Revenue apple production----
+  
+  #calculate the total apple yield
+  harvest_total<-orchard_area*trees_per_ha*harvest_tree
   
   
   #Calculate harvest loss> needs to be influenced by random events (scab_year, 
   #insect_year, vole_damage)
   #harvest_loss<-random_loss+scab_loss+insect_loss+occurence_vole_damage
   #DUMMY HARVEST LOSS####
-  dummy_harvest_loss<-harvest_total*0.2
-  marketable_harvest<-vv(harvest_total, var_CV, n=n_years)-dummy_harvest_loss
+  #dummy_harvest_loss<-harvest_total*0.2
   
-  #Revenue apple production----
+  #marketable_harvest<-vv(harvest_total, var_CV, n = n_years) - dummy_harvest_loss
   
-  Revenue_apple_production<-marketable_harvest*vv(apple_price, var_CV,n=n_years)
+  Revenue_apple_production <- marketable_harvest * vv(apple_price, var_CV, n = n_years)
   
   #Result_apple_production----
   
@@ -177,9 +159,9 @@ n_sim <- 10000
 # To get a probabilistic overview we run a Monte Carlo Simulation ####
 
 Apple_Simulation <- mcSimulation(estimate_read_csv("data_chicken_apple.csv"),
-                                         model_function = Apple_Simulation,
-                                         numberOfModelRuns = n_sim,
-                                         functionSyntax = "plainNames")
+                                 model_function = Apple_Simulation,
+                                 numberOfModelRuns = n_sim,
+                                 functionSyntax = "plainNames")
 # This fuction is to plot the distribution of values ####
 decisionSupport::plot_distributions(mcSimulation_object = Apple_Simulation, 
                                     vars = c("NPV"),
@@ -210,110 +192,110 @@ compound_figure(mcSimulation_object = Apple_Simulation, input_table = NULL,
                 cashflow_var_name = "Cashflow", base_size = 11)
 
 #Here we are writing the chicken in apple function----
-  
+
 Chicken_Apple_Simulation<- function(){
-    
+  
   #Some costs of the apple plantation are reduced due to benefits of including 
   #chicken into the plantation----
+  
+  cost_diesel<-consumption_diesel_hour*diesel_price
+  cost_harvesting<-(vv(yearly_harvesting_hours, var_CV, n=n_years)*
+                      hourly_wage)+
+    (vv(yearly_harvesting_hours, var_CV, n=n_years)*
+       cost_diesel)
+  
+  reduced_weeding<-
+    cost_weeding-vv((cost_weeding*0.15), var_CV, n=n_years)
+  
+  reduced_mowing<-
+    cost_mowing-vv((cost_mowing*0.15), var_CV, n=n_years)
+  
+  reduced_scabcontrol<-
+    cost_scabcontrol-vv((cost_scabcontrol*0.18), var_CV, n=n_years)
+  
+  reduced_cost_scab_year<-
+    chance_event(0.125,scab_year_treatment, 0, n=n_years, 0.25)
+  
+  reduced_insectcontrol<-
+    cost_insectcontrol-vv((cost_insectcontrol*0.2), var_CV, n=n_years)
+  
+  #funfact: regular mineral fertiliser contains 70% chicken manure!
+  reduced_nutrients<-
+    cost_nutrients-vv((cost_nutrients*0.7), var_CV, n=n_years)
+  
+  reduced_vole_control<-
+    cost_vole_control-vv((cost_vole_control*0.4), var_CV, n=n_years)
+  
+  reduced_occurence_vole_damage<-
+    chance_event(0.01, dead_tree_occurrence, 0, n=n_years)
+  
+  for(i in 1:n_years){
     
-    cost_diesel<-consumption_diesel_hour*diesel_price
-    cost_harvesting<-(vv(yearly_harvesting_hours, var_CV, n=n_years)*
-                        hourly_wage)+
-      (vv(yearly_harvesting_hours, var_CV, n=n_years)*
-         cost_diesel)
-    
-    reduced_weeding<-
-      cost_weeding-vv((cost_weeding*0.15), var_CV, n=n_years)
-    
-    reduced_mowing<-
-      cost_mowing-vv((cost_mowing*0.15), var_CV, n=n_years)
-    
-    reduced_scabcontrol<-
-      cost_scabcontrol-vv((cost_scabcontrol*0.18), var_CV, n=n_years)
-    
-    reduced_cost_scab_year<-
-      chance_event(0.125,scab_year_treatment, 0, n=n_years, 0.25)
-    
-    reduced_insectcontrol<-
-      cost_insectcontrol-vv((cost_insectcontrol*0.2), var_CV, n=n_years)
-    
-    #funfact: regular mineral fertiliser contains 70% chicken manure!
-    reduced_nutrients<-
-      cost_nutrients-vv((cost_nutrients*0.7), var_CV, n=n_years)
-    
-    reduced_vole_control<-
-      cost_vole_control-vv((cost_vole_control*0.4), var_CV, n=n_years)
-    
-    reduced_occurence_vole_damage<-
-      chance_event(0.01, dead_tree_occurrence, 0, n=n_years)
-    
-    for(i in 1:n_years){
+    if(reduced_occurence_vole_damage[i] == 1){
+      #if vole damage happens, some trees need to be replaced and affected trees 
+      #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
       
-      if(reduced_occurence_vole_damage[i] == 1){
-        #if vole damage happens, some trees need to be replaced and affected trees 
-        #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
-        
-        #harvest_total[i]<-harvest_total-vv(dead_trees, var_CV, n=1)*vv(harvest_tree, var_CV, n=1)
-        
-        cost_replacement_trees[i]<-cost_tree*dead_trees+planting_hours_tree*dead_tree*hourly_wage
-      }
+      #harvest_total[i]<-harvest_total-vv(dead_trees, var_CV, n=1)*vv(harvest_tree, var_CV, n=1)
       
-    }  
-    
-    #here we need to think again... less damage less loss.
-    #scab_year_treatment<-(vv(yearly_scabcontrol_hours, var_CV, n=1)*0.3*
-                            #hourly_wage)+
-      #cost_fungicide*0.3*
-      #orchard_area*2/3+
-      #yearly_scabcontrol_hours*0.3*
-      #cost_diesel  
-    #reduced_cost_scab_year<-chance_event(0.25,scab_year_treatment, 0, n=n_years, 0.25)
-    
-
-    #same here like scab
-    #insect_year_treatment<-yearly_insectcontrol_hours*0.3*
-     # hourly_wage+
-      #cost_insecticide*0.3*
-      #orchard_area*2/3+
-      #yearly_insectcontrol_hours*0.3*
-      #cost_diesel  
-    #cost_insect_year<-chance_event(0.25,insect_year_treatment, 0, n=n_years, 0.25)
-    
-    #Sum up all costs----
-    Costs_apple_production<-
-      cost_harvesting+
-      reduced_mowing+
-      reduced_weeding+
-      reduced_insectcontrol+
-      #cost_insect_year+
-      reduced_scabcontrol+
-      #cost_scab_year+
-      reduced_vole_control+
-      reduced_nutrients+
-      reduced_occurence_vole_damage
-    for(i in 1:n_years){
-      
-      if(reduced_occurence_vole_damage[i] == 1){
-        #if vole damage happens, some trees need to be replaced and affected trees 
-        #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
-        
-        cost_replacement_trees[i]<-
-          cost_tree*dead_trees+
-          planting_hours_tree*dead_trees*hourly_wage
-      }
-      
+      cost_replacement_trees[i]<-cost_tree*dead_trees+planting_hours_tree*dead_tree*hourly_wage
     }
-    #Calculate harvest loss
-    #harvest_loss<-random_loss+scab_loss+insect_loss+occurence_vole_damage
-    #DUMMY HARVEST LOSS####
-    dummy_harvest_loss<-harvest_total*0.2
-    marketable_harvest<-harvest_total-dummy_harvest_loss
     
-    #Revenue apple production----
+  }  
+  
+  #here we need to think again... less damage less loss.
+  #scab_year_treatment<-(vv(yearly_scabcontrol_hours, var_CV, n=1)*0.3*
+  #hourly_wage)+
+  #cost_fungicide*0.3*
+  #orchard_area*2/3+
+  #yearly_scabcontrol_hours*0.3*
+  #cost_diesel  
+  #reduced_cost_scab_year<-chance_event(0.25,scab_year_treatment, 0, n=n_years, 0.25)
+  
+  
+  #same here like scab
+  #insect_year_treatment<-yearly_insectcontrol_hours*0.3*
+  # hourly_wage+
+  #cost_insecticide*0.3*
+  #orchard_area*2/3+
+  #yearly_insectcontrol_hours*0.3*
+  #cost_diesel  
+  #cost_insect_year<-chance_event(0.25,insect_year_treatment, 0, n=n_years, 0.25)
+  
+  #Sum up all costs----
+  Costs_apple_production<-
+    cost_harvesting+
+    reduced_mowing+
+    reduced_weeding+
+    reduced_insectcontrol+
+    #cost_insect_year+
+    reduced_scabcontrol+
+    #cost_scab_year+
+    reduced_vole_control+
+    reduced_nutrients+
+    reduced_occurence_vole_damage
+  for(i in 1:n_years){
     
-    Revenue_apple_producion<-marketable_harvest*apple_price  
- 
- #Costs chicken----
+    if(reduced_occurence_vole_damage[i] == 1){
+      #if vole damage happens, some trees need to be replaced and affected trees 
+      #will die and have no fruits to be harvested. Thus, harvest needs to be adjusted. 
+      
+      cost_replacement_trees[i]<-
+        cost_tree*dead_trees+
+        planting_hours_tree*dead_trees*hourly_wage
+    }
+    
+  }
+  #Calculate harvest loss
+  #harvest_loss<-random_loss+scab_loss+insect_loss+occurence_vole_damage
+  #DUMMY HARVEST LOSS####
+  dummy_harvest_loss<-harvest_total*0.2
+  marketable_harvest<-harvest_total-dummy_harvest_loss
+  
+  #Revenue apple production----
+  
+  Revenue_apple_producion<-marketable_harvest*apple_price  
+  
+  #Costs chicken----
   
   cost_coop <- c(coop_invest * number_hens, rep(0, n_years -1))
   
@@ -330,7 +312,7 @@ Chicken_Apple_Simulation<- function(){
   
   cost_insurance <- vv(chicken_insurance, var_CV = var_CV, n = n_years)
   
-
+  
   #also right now we assume that we buy x amount of chicken and that they 
   #instantly die, dont eat and lay no eggs
   #is there a way to let them die halfway through the year
@@ -360,7 +342,7 @@ Chicken_Apple_Simulation<- function(){
   
   #>for the veterinary
   costs_vet<-cost_vet_visit+ costs_vaccin*number_hens+
-              chance_event(0.25, cost_vet_visit, 0, n=n_years)  
+    chance_event(0.25, cost_vet_visit, 0, n=n_years)  
   
   
   #revenues----
@@ -411,7 +393,7 @@ Chicken_Apple_Simulation<- function(){
       
       insurance_coverage[i] <- cost_flock[i] + revenue_eggs[i] + revenue_meat[i]
     }
-
+    
   }
   
   #add the different cost and benefit variables up
@@ -450,7 +432,7 @@ Chicken_Apple_Simulation <- mcSimulation(estimate_read_csv("data_chicken_apple.c
 # This fuction is to plot the distribution of values ####
 decisionSupport::plot_distributions(mcSimulation_object = Chicken_Apple_Simulation, 
                                     vars = c("NPV"),
-                              
+                                    
                                     # You can even add more results here
                                     method = "smooth_simple_overlay", 
                                     base_size = 11)
